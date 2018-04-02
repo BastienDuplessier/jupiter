d1604d96e0f799b37575c0f4a905f8fd95dcd41e
Bastien Duplessier <bastien.duplessier@gmail.com>
2018-04-02 23:48:03 +0200

    go fmt

--- scripts/regroup_by_continent.go
+++ scripts/regroup_by_continent.go
@@ -1,79 +1,78 @@
 package main
 
 import (
-  "fmt"
-  "io"
-  "io/ioutil"
-  "encoding/csv"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"strconv"
 	"strings"
-  "strconv"
 )
 
 type Job struct {
-  profession_id string
-  contract_type string
-  name string
-  office_latitude float64
-  office_longitude float64
+	profession_id    string
+	contract_type    string
+	name             string
+	office_latitude  float64
+	office_longitude float64
 }
 
-
 type Profession struct {
-  id string
-  name string
-  category_name string
+	id            string
+	name          string
+	category_name string
 }
 
 func Check(e error) {
-  if e != nil {
-    panic(e)
-  }
+	if e != nil {
+		panic(e)
+	}
 }
 
-func ParseFloat64(str string)float64 {
-  res, err := strconv.ParseFloat(str, 64)
-  if err == nil {
-    return res
-  } else {
-    return -1
-  }
+func ParseFloat64(str string) float64 {
+	res, err := strconv.ParseFloat(str, 64)
+	if err == nil {
+		return res
+	} else {
+		return -1
+	}
 }
 
-func ReadCsv(filename string)*csv.Reader {
-  dat, err := ioutil.ReadFile(filename)
-  Check(err)
+func ReadCsv(filename string) *csv.Reader {
+	dat, err := ioutil.ReadFile(filename)
+	Check(err)
 
 	r := csv.NewReader(strings.NewReader(string(dat)))
 	r.Read() // Discard headers
-  return r
+	return r
 }
 
-func RecToJob(rec []string)Job {
-    lat := ParseFloat64(rec[3])
-    lon := ParseFloat64(rec[4])
-    return Job{rec[0], rec[1], rec[2], lat, lon}
+func RecToJob(rec []string) Job {
+	lat := ParseFloat64(rec[3])
+	lon := ParseFloat64(rec[4])
+	return Job{rec[0], rec[1], rec[2], lat, lon}
 }
 
-func ExtractJobs()[]Job {
-  var jobs []Job
+func ExtractJobs() []Job {
+	var jobs []Job
 
-  r := ReadCsv("data/jobs.csv")
+	r := ReadCsv("data/jobs.csv")
 	for {
 		rec, err := r.Read()
 		if err == io.EOF {
 			break
 		}
-    Check(err)
+		Check(err)
 
 		fmt.Println(rec)
-    jobs = append(jobs, RecToJob(rec))
+		jobs = append(jobs, RecToJob(rec))
 	}
 
-  return jobs
+	return jobs
 }
 
 func main() {
-  jobs := ExtractJobs()
+	jobs := ExtractJobs()
 
 	// Iterate through list and print its contents.
 	for i, job := range jobs {
