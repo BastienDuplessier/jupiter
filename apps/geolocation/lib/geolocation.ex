b55503409bcf3304c00e31a485146601839ad0ec
Bastien Duplessier <bastien.duplessier@gmail.com>
2018-04-03 05:45:54 +0200

    Add Geolocation app, get jobs by lat/lon/radius

---
+++ apps/geolocation/lib/geolocation.ex
@@ -0,0 +1,44 @@
+defmodule Geolocation do
+  @moduledoc """
+  Documentation for Geolocation.
+  """
+
+  @doc """
+    Return job offers inside a specified zone.
+  """
+  def compute(latitude, longitude, radius) do
+    all_jobs = :ets.match_object(:jobs, {:'_', :'$1'})
+    Enum.filter(all_jobs, fn({_, job}) ->
+      {lat, lon} = {job["office_latitude"], job["office_longitude"]}
+      dist = Distance.GreatCircle.distance({latitude, longitude}, {lat, lon})
+      dist < radius * 1000
+    end)
+  end
+                                
+  @doc """
+    Fills ETS table with data from jobs.csv
+  """                            
+  def init_jobs() do
+    :ets.new(:jobs, [:set, :protected, :named_table])
+    result = File.stream!("data/jobs.csv") |> CSV.decode(headers: true)
+    Enum.map(result, fn({:ok, val}) ->
+      lat = parse_latlon(val["office_latitude"])
+      lon = parse_latlon(val["office_longitude"])
+      job = Map.merge(val, %{"office_latitude" => lat, "office_longitude" => lon})
+      id = System.unique_integer [:monotonic, :positive]
+      :ets.insert(:jobs, {id, job})
+    end)
+  end
+
+
+  @doc """
+    Parse a string and extract the coord as float
+    Handle errors by returning 0
+  """
+  defp parse_latlon(str) do
+    case Float.parse(str) do
+      :error -> 0.0
+      {res, _} -> res
+    end
+  end
+end
---
+++ apps/geolocation/test/geolocation_test.exs
@@ -0,0 +1,8 @@
+defmodule GeolocationTest do
+  use ExUnit.Case
+  doctest Geolocation
+
+  test "greets the world" do
+    assert Geolocation.hello() == :world
+  end
+end
